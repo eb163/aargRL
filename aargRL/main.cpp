187ee10c1cb065ba9d681a52c8bcc11281b792f1
eb163 <ericbrun350@att.net>
2019-08-17 16:03:00 -0500

    testing colors

--- aargRL/main.cpp
+++ aargRL/main.cpp
@@ -1,10 +1,15 @@
 #include "pdcurses/curses.h"
+#include "pdcurses/curseutil.h"
+
+#include "tests.h"
 
 int main()
 {
 	initscr();
 	start_color();
-	//init_pair();
+	init_def_color_pairs();
+
+	testDefaultColorPairs();
 
 	return 0;
 }
--- aargRL/tests.h
+++ aargRL/tests.h
@@ -1,4 +1,44 @@
 #pragma once
 #include <assert.h>
+#include <string>
+#include "pdcurses/curses.h"
+#include "pdcurses/curseutil.h"
+
+#include "DefaultSettings.h"
 
 //various methods for testing code
+
+using std::string;
+
+void testDefaultColorPairs()
+{
+	//precondition: colors have been initiated
+	if (has_colors() == true)
+	{
+		WINDOW* testwin = newwin(DEF_SCR_BOUNDS_Y, DEF_SCR_BOUNDS_X, 0, 0);
+
+		wclear(testwin);
+
+		string white = "white", blue = "blue", red = "red", yellow = "yellow", green = "green", purple = "purple",
+			cyan = "cyan", black = "black";
+
+		mvwprintcolorstr(testwin, 1, 1, white, COLOR_WHITE);
+		mvwprintcolorstr(testwin, 2, 1, blue, COLOR_BLUE);
+		mvwprintcolorstr(testwin, 3, 1, red, COLOR_RED);
+		mvwprintcolorstr(testwin, 4, 1, yellow, COLOR_YELLOW);
+		mvwprintcolorstr(testwin, 5, 1, green, COLOR_GREEN);
+		mvwprintcolorstr(testwin, 6, 1, purple, COLOR_MAGENTA);
+		mvwprintcolorstr(testwin, 7, 1, cyan, COLOR_CYAN);
+		mvwprintcolorstr(testwin, 8, 1, black, COLOR_BLACK);
+
+		mvwaddstr(testwin, 10, 1, "Press any key to continue...");
+		
+		wrefresh(testwin);
+
+		overwrite(testwin, stdscr);
+
+		wrefresh(stdscr);
+
+		getch();
+	}
+}
