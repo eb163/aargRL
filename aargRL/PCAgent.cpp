8bf58ab7c6733aff67eb8dab7f70734714662c41
eb163 <ericbrun350@att.net>
2019-08-18 20:04:46 -0500

    defined a generic constructor

--- aargRL/PCAgent.cpp
+++ aargRL/PCAgent.cpp
@@ -2,9 +2,21 @@
 
 PCAgent::PCAgent()
 {
+	setType(AgentType::PC);
+	setName("Player");
+	setDescr("The daring hero of our story!");
+	gdata = new GraphicData('@', COLOR_WHITE);
+	coord.x = 0; coord.y = 0;
 
+	hp.setMax(100); //placeholder arbitrary value
+	hp.setCurrent(hp.getMax());
+
+	mp.setMax(100); //placeholder arbitrary value
+	mp.setCurrent(mp.getMax());
+
+	stomach.setMax(1000); //placeholder arbitrary value
+	stomach.setCurrent(stomach.getMax() / 2);
 }
-
 
 PCAgent::~PCAgent()
 {
--- aargRL/PCAgent.h
+++ aargRL/PCAgent.h
@@ -2,12 +2,38 @@
 #include "Agent.h"
 class PCAgent : public Agent
 {
+
 private:
+	//inherited
+	//AgentType type
 
 protected:
+	//inherited:
+	/*
+	string name;
+	string description;
+	GraphicData* gdata = nullptr; 
+	Pair<int> coord; 	//for tracking position on map
+	Pool hp; //health
+	Pool mp; //magicka or mana or spellpower or whatever (TBD)
+	Pool stomach; //metabolism
+	*/
 
 public:
 	PCAgent();
 	~PCAgent();
+
+	//inherited:
+	/*
+	void setName(string n);
+	string getName();
+
+	void setDescr(string d);
+	string getDescr();
+
+	Pair<int> getPosition();
+	void setPosition(int y, int x);
+	void move(int dy, int dx);
+	*/
 };
 
