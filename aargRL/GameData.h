dc8c86c74a9ce7d20e2a5b4d28d8ee1300f2ec0b
eb163 <ericbrun350@att.net>
2019-08-19 08:41:49 -0500

    created GameData class

---
+++ aargRL/GameData.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//a class for containing and managing data for the main game loop, including PCAgent, Map data, time and turn data, NPCAgents and EnemyAgents
+
+class GameData
+{
+private:
+
+protected:
+
+public:
+	GameData();
+	~GameData();
+};
+
