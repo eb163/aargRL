37eb9110e48fc17de7c66101b4ef9780a1bf4113
eb163 <ericbrun350@att.net>
2019-08-19 11:55:27 -0500

    started implementing GameData class

--- aargRL/GameData.cpp
+++ aargRL/GameData.cpp
@@ -1,12 +1,60 @@
 #include "GameData.h"
 
-
-
 GameData::GameData()
 {
+	rngSeed = time(0);
+	pcptr = nullptr;
+	turnCount = 0;
+	totalTime = 0;
 }
 
 
 GameData::~GameData()
 {
+
+}
+
+void GameData::setTurnCount(int t)
+{
+	turnCount = t;
+}
+
+void GameData::modifyTurnCount(int dt)
+{
+	turnCount += dt;
+}
+
+int GameData::getTurnCount()
+{
+	return turnCount;
+}
+
+void GameData::setTotalTime(time_t time)
+{
+	totalTime = time;
+}
+
+time_t GameData::getTotalTime()
+{
+	return totalTime;
+}
+
+void GameData::setRNGSeed(time_t time)
+{
+	rngSeed = time;
+}
+
+time_t GameData::getRNGSeed()
+{
+	return rngSeed;
+}
+
+void GameData::setPC(PCAgent * ptr)
+{
+	pcptr = ptr;
+}
+
+PCAgent * GameData::getPC()
+{
+	return pcptr;
 }
--- aargRL/GameData.h
+++ aargRL/GameData.h
@@ -1,15 +1,53 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <ctime>
+#include "PCAgent.h"
+//a class for containing and managing data for the main game loop, including PCAgent, Map data, Item data time and turn data, NPCAgents and EnemyAgents
 
-//a class for containing and managing data for the main game loop, including PCAgent, Map data, time and turn data, NPCAgents and EnemyAgents
+using std::string;
 
 class GameData
 {
 private:
+	int turnCount;
+	time_t totalTime;
+	time_t rngSeed;
+
+	//pc
+	PCAgent* pcptr;
+
+	//Map
+
+	//Items
+
+	//NPCs
+
+	//Enemies
+
 
 protected:
 
 public:
 	GameData();
 	~GameData();
+
+	void setTurnCount(int t);
+	void modifyTurnCount(int dt);
+	int getTurnCount();
+
+	void setTotalTime(time_t time);
+	time_t getTotalTime();
+	string getTotalTimeStr();
+
+	void setRNGSeed(time_t time);
+	time_t getRNGSeed();
+
+	void setPC(PCAgent* ptr);
+	PCAgent* getPC();
+
+	//save method: writes data from GameData to a binary file
+
+	//load method: loads data from a binary file into the calling GameData instance, overwriting any existing data in the instance
 };
 
