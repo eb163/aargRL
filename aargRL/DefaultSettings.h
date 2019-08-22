adc3e59705cac979c35ed4c191f97c99237d25f3
eb163 <ericbrun350@att.net>
2019-08-22 09:38:21 -0500

    added some more default settings

--- aargRL/DefaultSettings.h
+++ aargRL/DefaultSettings.h
@@ -2,3 +2,10 @@
 
 #define DEF_SCR_BOUNDS_X 80
 #define DEF_SCR_BOUNDS_Y 25
+//^^ default window size on initialization does not match up with this
+
+#define MAP_SCR_BOUNDS_X 80
+#define MAP_SCR_BOUNDS_y 25
+
+#define HUD_SCR_BOUNDS_X 80
+#define HUD_SCR_BOUNDS_Y 6
