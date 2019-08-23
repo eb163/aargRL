2999f2dcb99a5ddf3dc71dfb1213138f84754ec8
eb163 <ericbrun350@att.net>
2019-08-23 14:35:27 -0500

    defined some examples of food and a function for randomly making a particular food

--- aargRL/Food.cpp
+++ aargRL/Food.cpp
@@ -4,9 +4,51 @@ Food::Food()
 {
 	setItemType(ItemType::CONSUMABLE);
 	setConsumableType(ConsumableType::FOOD);
-	setGraphicData('%', COLOR_YELLOW);
+	randomize();
 }
 
 Food::~Food()
 {
+}
+
+int Food::getNutrition()
+{
+	return nutrition;
+}
+
+void Food::setNutrition(int n)
+{
+	nutrition = n;
+}
+
+void Food::randomize()
+{
+	int num = rand() % totalFoodTypes;
+
+	switch (num)
+	{
+	case BREAD:
+		setName("hardtack");
+		setDescr("A type of simple long lasting bread made by mixing 2 parts flour, one part water, and salt, and baking until hard and dry.");
+		setNutrition(100); //arbitrary for now
+		setGraphicData('%', COLOR_YELLOW);
+		break;
+	case MEAT:
+		setName("dried meat");
+		setDescr("A piece of meat that's been dried and salted.");
+		setNutrition(200); //arbitrary for now
+		setGraphicData('%', COLOR_RED);
+		break;
+	case ELFBREAD:
+		setName("elven wafer");
+		setDescr("A traditional traveling ration made by the elves. These wafers are prized for their light weight and high nutritional content.");
+		setNutrition(400); //arbitrary for now
+		setGraphicData('%', COLOR_WHITE);
+	case DWARFBREAD:
+		setName("dwarven biscuit");
+		setDescr("A traditional traveling ration made by the dwarves. These biscuits have a tough crunchy crust, light fluffy interior, and a faint taste of fine dwarven beer.");
+		setNutrition(200);
+		setGraphicData('%', COLOR_YELLOW);
+		break;
+	}
 }
--- aargRL/Food.h
+++ aargRL/Food.h
@@ -1,13 +1,23 @@
 #pragma once
 #include "Consumable.h"
+
+enum FoodType {BREAD = 0, MEAT = 1, ELFBREAD = 2, DWARFBREAD = 3};
+
 class Food : public Consumable
 {
 private:
+	static const int totalFoodTypes = 4;
+	int nutrition; //when consumed, nutrition value is added to consumer's metabolism
 
 protected:
 
 public:
 	Food();
 	~Food();
+
+	int getNutrition();
+	void setNutrition(int n);
+
+	void randomize(); //used when generating a new food object, or transmogrifying an existing food object
 };
 
