c1ef7c7cce37a50cbb2746214ab2383e59d550ca
eb163 <ericbrun350@att.net>
2019-08-25 13:45:06 -0500

    added comments on inherited attr/methods

--- aargRL/Food.h
+++ aargRL/Food.h
@@ -8,7 +8,25 @@ class Food : public Consumable
 private:
 	int nutrition; //when consumed, nutrition value is added to consumer's metabolism
 
+
 protected:
+	/*
+	//inherited
+	void setItemType(ItemType t);
+
+	void setConsumableType(ConsumableType t);
+
+	string name;
+	string description;
+
+	GraphicData gdata;
+
+	Pair<int> coord;
+	//for tracking position on map
+
+	int weight;
+
+	*/
 
 public:
 	Food();
@@ -22,5 +40,28 @@ public:
 	void makeFood(FoodType type);
 
 	void randomize(); //used when generating a new unspecific food object, or transmogrifying an existing food object
+
+	/*
+	//inherited
+	
+	ItemType getItemType();
+
+	ConsumableType getConsumableType();
+
+	void setGraphicData(char ch, int color_pair);
+	GraphicData getGraphicData();
+
+	void setName(string n);
+	string getName();
+
+	void setDescr(string d);
+	string getDescr();
+
+	void setPosition(int y, int x);
+	Pair<int> getPosition();
+
+	void setWeight(int n);
+	int getWeight();
+	*/
 };
 
